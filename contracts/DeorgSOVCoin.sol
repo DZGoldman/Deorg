efa9faee5019a4e651a5513de29d20687eae4691
dzgoldman <dzgoldman@wesleyan.edu>
2021-07-11 11:39:42 -0400

    include mintDeorgReward

--- contracts/DeorgSOVCoin.sol
+++ contracts/DeorgSOVCoin.sol
@@ -6,7 +6,32 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract DeorgSOV is ERC20 {
-    constructor() public ERC20("DeorgSOV", "DSOV") {
+    address deorgAddress;
+    uint256 constant supplyCap  = 21000000000000000000000000;
+    constructor() public ERC20("DeorgSOV", "DSOV") {}
+
+    function init (address _deorgAddress) external returns(bool) {
+        require(deorgAddress != address(0), "ALREADY_INITED");
+        deorgAddress = _deorgAddress;
+        return true;
+    }
+
+    function mintDeorgReward(address[] memory beneficiaries) public onlyFromDeorg returns(bool)  {
+        require(beneficiaries.length > 0, "NEED_BENEFICIARIES");
+        // uint rewardPerBenficiary = (supplyCap - totalSupply) * 0.1 / beneficiaries.length;
+        uint rewardPerBenficiary = (supplyCap - totalSupply()) * 1/(10 * beneficiaries.length);
+        for (uint256 i = 0; i < beneficiaries.length; i++) {
+            _mint(beneficiaries[i], rewardPerBenficiary);
+        }
+
+        require(totalSupply() <= supplyCap, "Supply cap superceded somehow; must prevent at all costs, sorry");
+
+        return true;
+    }   
+
+    modifier onlyFromDeorg(){
+        require(msg.sender == deorgAddress, "NOT_FROM_DEORG");
+        _;
     }
 }
 
